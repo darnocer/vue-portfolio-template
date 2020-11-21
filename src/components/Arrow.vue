d073eaffa58230934abe15e975e38fc80851649d
darnocer <darnocer@gmail.com>
2020-11-21 09:30:27 -0700

    navigation arrow

---
+++ src/components/Arrow.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row arrow-container">
+      <a href="#about">
+    <i class="fas fa-angle-double-down fa-6x"></i></a>
+  </div>
+ 
+</template>
+
+<script>
+import data from "../data/data.json";
+
+export default {
+  name: 'Arrow',
+  props: {
+  },
+  data() {
+    return {
+      data: data,
+      }
+    }
+
+}
+</script>
+
+<style lang="scss">
+@import "../styles/scss/_variables.scss";
+
+
+</style>
