dcd2123d8d9bdff27d5a6b1bd494237d5693ec6d
darnocer <darnocer@gmail.com>
2020-11-21 09:29:51 -0700

    highlight nav item on scroll

--- src/main.js
+++ src/main.js
@@ -1,8 +1,50 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import $ from "jquery";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
+
+// HIGHLIGHT NAV MENU ITEM ON SCROLL
+// Cache selectors
+var lastId,
+  topMenu = $("#navbar-nav"),
+  topMenuHeight = topMenu.outerHeight() + 1,
+  // // All list items
+  menuItems = topMenu.find("a"),
+  // // Anchors corresponding to menu items
+  scrollItems = menuItems.map(function() {
+    var item = $($(this).attr("href"));
+    if (item.length) {
+      return item;
+    }
+  });
+
+// Bind to scroll
+$(window).on("scroll", function() {
+  // Get container scroll position
+  var fromTop = $(this).scrollTop() + topMenuHeight;
+
+  // Get id of current scroll item
+  var cur = scrollItems.map(function() {
+    if ($(this).offset().top < fromTop) return this;
+  });
+  // Get the id of the current element
+  cur = cur[cur.length - 1];
+  var id = cur && cur.length ? cur[0].id : "";
+
+  if (lastId !== id) {
+    lastId = id;
+    // Set/remove active class
+    menuItems
+      .parent()
+      .removeClass("active")
+      .end()
+      .filter("[href=\\#" + id + "]")
+      .parent()
+      .addClass("active");
+  }
+});
--- src/pages/Header.vue
+++ src/pages/Header.vue
@@ -1,33 +1,35 @@
 <template>
-<header class="bg">
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">dariannocera.</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+<header class="bg" id="home">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand" href="#">dariannocera.</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav ml-auto">
-      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#about">About</a>
-      <a class="nav-item nav-link" href="#skills">Skills</a>
-      <a class="nav-item nav-link" href="#projects">Projects</a>
-      <a class="nav-item nav-link" href="#contact">Contact</a>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <ul class="navbar-nav ml-auto" id="navbar-nav">
+        <li><a class="nav-item nav-link" href="#home">Home</a></li>
+        <li><a class="nav-item nav-link" href="#about">About</a></li>
+        <li><a class="nav-item nav-link" href="#skills">Skills</a></li>
+        <li><a class="nav-item nav-link" href="#projects">Projects</a></li>
+        <li><a class="nav-item nav-link" href="#contact">Contact</a></li>
+      </ul>
     </div>
-  </div>
-</nav>
+  </nav>
 <Hero/>
+<Arrow />
 </header>
 </template>
 
 <script>
-
+import Arrow from '../components/Arrow.vue';
 import Hero from '../components/Hero.vue'
 import data from "../data/data.json";
 
 export default {
   name: 'Header',
   components: {
-    Hero
+    Hero,
+   Arrow
   },
   props: {
   },
@@ -40,6 +42,6 @@ export default {
 </script>
 
 <style lang="scss">
-@import "../styles/scss/_variables.scss";
+
 
 </style>
