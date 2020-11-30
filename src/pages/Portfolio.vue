1ca1c9c9767deeab9e4af5a1a0fcd9d2dc3ef93b
darnocer <darnocer@gmail.com>
2020-11-30 14:30:44 -0700

    skills layout

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -2,18 +2,20 @@
   <section id="portfolio" class="dark-section">
     <div class="container-fluid">
       <h1>{{heading}}</h1>
-      <div class="row">
-        <ul class="list-inline mx-auto filters">
+
+      <!-- start of filters  -->
+      <div class="row filters">
+        <ul class="list-inline mx-auto">
           <li v-for="item in filters" :key="item.title" class="list-inline-item filter">
             <a class="nav-item" :class="(item.filter === currentFilter) ? 'active': null" :data-filter="item.filter" @click="setFilter">{{item.filter}}</a>
           </li>
         </ul>
       </div>
+      <!-- end of filters  -->
 
-      <div class="portfolio-container row">
+      <div id="portfolio-container" class="row">
         <div data-aos="fade-right" data-aos-duration="1000" v-for="(item, index) in filteredProjects" :key="index"  class="col-sm-12 col-md-4 flex-col">
           <div class="portfolio-item">
-            
             <img :src="require(`../assets/images/portfolio/${item.image}`)" />
             
             <div class="overlay">
@@ -43,9 +45,6 @@
 import data from "../data/data.json";
 import Arrow from "../components/Arrow.vue";
 
-
-var filterArr = [];
-
 export default {
   name: 'portfolio',
   props: {},
@@ -64,7 +63,6 @@ export default {
         var projects = data.portfolio.projects;
         var filter = this.currentFilter;
         var filtered = projects.filter(function(x) {
-          
             return x.filter === filter;
           });
         return filtered;
@@ -82,18 +80,10 @@ export default {
       }
     },
   methods: {
-    displayFilters(item) {
-      if (!filterArr.includes(item.filter)){
-        filterArr.push(item.filter);
-        return true;
-      }
-      return false;
-    },
-   
     setFilter(event){
       this.currentFilter = event.target.dataset.filter;
-      filterArr =[];
-    },
+    
+    }
   
   }
    
--- src/pages/Skills.vue
+++ src/pages/Skills.vue
@@ -4,48 +4,44 @@
       <h1>{{heading}}</h1>
 
       <!-- start of filters  -->
-      <div class="row">
-      <ul class="list-inline mx-auto filters" data-aos="fade-right" data-aos-duration="1000">
+      <div class="row filters">
+      <ul class="list-inline mx-auto" data-aos="fade-right" data-aos-duration="1000">
           <li v-for="item in skills" :key="item.category" class="list-inline-item filter">
-            <a class="nav-item" :class="(item.category === currentCategory) ? 'active': null" :data-category="item.category" @click="setCategory" data-toggle="collapse" :href="'#'+ categoryId(item)" role="a" aria-expanded="false" :aria-controls="categoryId(item)">{{item.category}}</a>
+            <a class="nav-item" :class="(item.category === currentCategory) ? 'active': null" :data-category="item.category" @click="setCategory">{{item.category}}</a>
           </li>
        </ul>
        </div>
       <!-- end of filters  -->
 
       <!-- start of skill container  -->
-      <div id="skill-container">
-        <!-- start of individual skill container  -->
-        <div data-parent="#skill-container" v-for="item in skills" :key="item.category" class="collapse multi-collapse" :id="categoryId(item)">
-          <!-- start of category data  -->
-          <div class="category">
-            <h2><i :class="item.faClass"></i> {{item.category}}</h2>
+        <div id="skill-container" data-aos="fade-right" data-aos-duration="1000" v-for="item in filteredSkills" :key="item.category">
+            <div class="category-heading">
+              <h2><i :class="item.faClass"></i> {{item.category}}</h2>
               <h3>{{item.subtitle}}</h3>
-              <div class="skill-description">
-                <ul v-if="item.bullets.length !==0" class="fa-ul">
-                  <li v-for="bullet in item.bullets" :key="bullet"><span class="fa-li"><i class="fas fa-check"></i></span>{{bullet}}</li>
-                </ul>
-              </div>
+            </div>
 
+          <div class="skill-content">
+            <div v-if="item.bullets.length" class="skill-description">
+              <ul class="fa-ul">
+                <li v-for="bullet in item.bullets" :key="bullet"><span class="fa-li"><i class="fas fa-check"></i></span>{{bullet}}</li>
+                </ul>
+            </div>
 
           <!-- start of skill bars  -->
-              <div v-if="item.skillList.length !== 0" class="skillbars">
-                <div class="skill" v-for="skill in item.skillList" :key="skill.name" >
-                  <h3 class="skill-name" v-html="skill.name"></h3>
-                    <div class="outer-bar">
-                      <div :class="'inner-bar level--'+ skill.level"></div>
-                    </div>
-                </div>
-              </div>  
+            <div v-if="item.skillList.length !== 0" class="skillbars">
+              <div class="skill" v-for="skill in item.skillList" :key="skill.name" >
+                <h3 class="skill-name" v-html="skill.name"></h3>
+                  <div class="outer-bar">
+                    <div :class="'inner-bar level--'+ skill.level"></div>
+                  </div>
+              </div>
+            </div>  
           <!-- end of skill bars  -->
-          </div>
-          <!-- end of category data  -->
-        </div>
-        <!-- end of individual skill container  -->   
+         </div>
       </div>
   <!-- end of skill container  -->
 </div>
- <!-- End of Container  -->
+ <!-- end of main container  -->
     <Arrow />
   </section>
 </template>
@@ -62,18 +58,29 @@ export default {
     },
   data() {
     return {
-      skills: data.skills,
+      skills: data.skills.categories,
       heading: data.main.headings.skills,
-      currentCategory: null
+      currentCategory: data.skills.defaultCategory
     };
+  },
+  computed: {
+    filteredSkills() {
+      var skills = data.skills.categories;
+      var category = this.currentCategory;
+      var filtered = skills.filter(function(x) {
+          if (x.category === category) {
+            return x;
+          }
+          });
+
+          console.log(filtered);
+
+        return filtered;
+    }
   },
   methods: {
     setCategory(event){
       this.currentCategory = event.target.dataset.category;
-    },
-    categoryId(item){
-      var str = item.category;
-      return str.toLowerCase().split(" ").join("");
     }
   }
 };
