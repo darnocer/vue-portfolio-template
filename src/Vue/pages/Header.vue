d9172621d9581d3fcdf1d23d7b21331522ce8da6
darnocer <darnocer@gmail.com>
2020-12-17 13:16:04 -0700

    fixing hamburger icon and active nav

--- src/Vue/pages/Header.vue
+++ src/Vue/pages/Header.vue
@@ -16,11 +16,11 @@
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon"><i class="fas fa-bars" style="color:#fff; font-size:28px;"></i></span>
       </button>
       <div id="navbarNavAltMarkup" class="collapse navbar-collapse">
         <ul id="navbar-nav" class="navbar-nav ml-auto">
-          <li class="active">
+          <li class="active-nav">
             <a class="nav-item nav-link" href="#home">Home</a>
           </li>
           <li><a class="nav-item nav-link" href="#about">About</a></li>
