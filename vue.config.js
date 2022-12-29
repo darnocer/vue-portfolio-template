a0c3c8caa665eaa928b4f71fd0a7fe834ce71a24
root <root@DESKTOP-AQI9N9K>
2022-12-29 18:14:43 -0500

    fixed hot reload

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,24 @@
+
 module.exports = {
   configureWebpack: {
     devServer: {
       watchOptions: {
-        poll: true
+        poll: true,
+        ignored: /node_modules/
       }
     }
+  },
+
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [
+        __dirname + '/src/shared/assets/styles/vars/_all.scss'
+      ]
+    }
   }
+
+
+
+
 }
